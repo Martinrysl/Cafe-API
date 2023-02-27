711a5138d83eb7a3877438fae1e581d395abb4c7
Martinrysl <martinreyez32@gmail.com>
2023-02-27 12:49:19 -0600

    Post and Patch Updated

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from random import randint
+from sqlalchemy.sql.expression import func
+import sqlite3
 
 app = Flask(__name__)
 
 ##Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/martin/PycharmProjects/CAFE-API/cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -24,6 +26,21 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def __repr__(self):
+        return '<Cafe %r>' % self.name
+
+    def to_dict(self):
+        # Method 1.
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+
 
 @app.route("/")
 def home():
@@ -32,18 +49,64 @@ def home():
 
 @app.route("/random")
 def random():
-    cafe = Cafe()
-    cafe_select = cafe.query.where(cafe.columns.name == 'Old Spike')
+    query = Cafe.query
+    cafe = query.get(randint(1, query.count()))
+    params = {column.name: cafe.__getattribute__(column.name) for column in Cafe.__table__.columns}
 
-    return cafe_select
+    return jsonify(params)
 
-## HTTP GET - Read Record
 
-## HTTP POST - Create Record
+@app.route("/all")
+def all():
+    temp_db = sqlite3.connect("cafes.db")
+    temp_db.row_factory = sqlite3.Row
+    values = temp_db.execute("SELECT * FROM cafe").fetchall()
+    list_accumulator = []
+    for item in values:
+        list_accumulator.append({k: item[k] for k in item.keys()})
 
-## HTTP PUT/PATCH - Update Record
+    temp_db.close()
+    return jsonify(list_accumulator)
 
-## HTTP DELETE - Delete Record
+@app.route("/search/")
+def search():
+    query_location = request.args.get("loc")
+    cafe = db.session.query(Cafe).filter_by(location=query_location).first()
+    if cafe:
+        return jsonify(cafe=cafe.to_dict())
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
+
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def patch(cafe_id):
+    new_price = request.args.get("new_price")
+    cafe = db.session.query(Cafe).get(cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the price."})
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
 
 
 if __name__ == '__main__':
